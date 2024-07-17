96ca875134d26880d79849a184ee1a7b92cfa450
vivek-panchal <vivek.nsut@gmail.com>
2024-07-17 20:16:40 +0530

    chore: Update API_URL in utils.js to use production backend URL

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://task-manager-backend-rho.vercel.app';
+export const API_URL = 'https://task-manager-backend-drab.vercel.app';
